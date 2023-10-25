062b93a185c0a881650092a846f25000aa2e4de9
ravel01106 <ravello2002@gmail.com>
2023-10-25 09:56:27 +0100

    refactor: we create an array with the features and show it  using the map function

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,41 +1,56 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import React from "react";
 
 export default function Features() {
+  const features = [
+    "Salir a pasear",
+    "Senderismo",
+    "Ir a la playita",
+    "Domingos de misa",
+    "La guitarrita",
+    "El monte con lluvia",
+    "Viajar",
+    "Música variadita",
+    "Anime",
+    "Ducharme",
+    "Videojuegos",
+    "Ir de cenar romántica",
+  ];
   return (
     <View>
-    <Text style= {{color: 'beriblak', fontWeight: "900", textTransform: 'capitalize', fontSize: 20, textAlign: 'center'}}>
-      cosas que me gustan mucho:
-   </Text>
-    <ScrollView style={{padding: 10}}>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>Salir a pasear</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>Senderismo</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>Ir a la playita</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>Domingos de misa</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>La guitarrita</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>El monte con lluvia</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>Viajar</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>Música variadita</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>Anime</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>Ducharme</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>Videojuegos</Text>
-      <Text style={styles.cosasQmeGustanMuxoEstails}>Ir de cenar romántica</Text>
-    </ScrollView>
-  </View>
-  )
+      <Text style={styles.title}>cosas que me gustan mucho:</Text>
+      <ScrollView style={styles.containerFeatures}>
+        {features.map((feature, index) => (
+          <Text key={index} style={styles.feature}>
+            {feature}
+          </Text>
+        ))}
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    cosasQmeGustanMuxoEstails: {
-        borderColor: 'black',
-        borderWidth: 1,
-        borderStyle: 'dashed',
-        padding: 20,
-        color: 'darkred',
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontStyle: 'italic',
-        fontSize: 16,
-        backgroundColor: 'silver'
-      }
-})
+  feature: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    padding: 20,
+    color: "darkred",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontStyle: "italic",
+    fontSize: 16,
+    backgroundColor: "silver",
+  },
+  title: {
+    color: "black",
+    fontWeight: "900",
+    textTransform: "capitalize",
+    fontSize: 20,
+    textAlign: "center",
+  },
+  containerFeatures: {
+    padding: 10,
+  },
+});
