eda45d09255c9ae4da63047ec9e4862b7f5d4513
ravel01106 <amandaravelocabrera@gmail.com>
2023-10-31 09:18:42 +0000

    feat(Header): We put an icon next to the switch that is changed on depend of the theme

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,63 @@
 import { StyleSheet, Text, View, Pressable, Switch } from "react-native";
-import React, { useState } from "react";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { colors, colorsDark } from "../assets/color/Color";
 import { HeaderProps } from "../assets/interfaces/headerprops";
 
 export default function Header(props: HeaderProps) {
-  const { setDisplayPortfolioAmanda ,setIsEnabled,  isEnabled} = props;
+  const { setDisplayPortfolioAmanda, setIsEnabled, isEnabled } = props;
   const toggleSwitch = () => setIsEnabled(!isEnabled);
 
   const handleChange = (election: boolean): void => {
     setDisplayPortfolioAmanda(election);
   };
 
-
   return (
-    <View style={[styles.container, isEnabled ? {backgroundColor: colors.primary} : {backgroundColor: colorsDark.primary}]}>
+    <View
+      style={[
+        styles.container,
+        isEnabled
+          ? { backgroundColor: colors.primary }
+          : { backgroundColor: colorsDark.primary },
+      ]}
+    >
       <View style={styles.containerTitle}>
         <Text style={styles.title}>My Portfolio App</Text>
-        <Switch
-          trackColor={{false: '#767577', true: '#81b0ff'}}
-          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleSwitch}
-          value={isEnabled}
-        >
-        </Switch>
+        <View style={styles.containerSwitch}>
+          <Ionicons
+            name={isEnabled ? "moon-sharp" : "sunny"}
+            size={24}
+            color={colors.text}
+          />
+          <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          ></Switch>
+        </View>
       </View>
       <View style={styles.navbar}>
         <Pressable
           onPress={() => handleChange(true)}
-          style={[styles.buttonStyles, isEnabled ? {backgroundColor: colors.secondary} : {backgroundColor: colorsDark.secondary}]}
+          style={[
+            styles.buttonStyles,
+            isEnabled
+              ? { backgroundColor: colors.secondary }
+              : { backgroundColor: colorsDark.secondary },
+          ]}
         >
           <Text style={styles.buttonsText}>PORTAFOLIO AMANDA</Text>
         </Pressable>
         <Pressable
           onPress={() => handleChange(false)}
-          style={[styles.buttonStyles, isEnabled ? {backgroundColor: colors.secondary} : {backgroundColor: colorsDark.secondary}]}
+          style={[
+            styles.buttonStyles,
+            isEnabled
+              ? { backgroundColor: colors.secondary }
+              : { backgroundColor: colorsDark.secondary },
+          ]}
         >
           <Text style={styles.buttonsText}>PORTAFOLIO VALLEJO</Text>
         </Pressable>
@@ -50,23 +73,24 @@ const styles = StyleSheet.create({
     width: "100%",
     color: colors.text,
   },
-
+  containerSwitch: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
   containerTitle: {
     marginVertical: 15,
-    flexDirection: 'row',
-    justifyContent: 'space-around'
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
-
   buttonStyles: {
     padding: 10,
     borderRadius: 10,
   },
-
   buttonsText: {
     color: colors.text,
     fontWeight: "bold",
   },
-
   title: {
     textAlign: "center",
     fontWeight: "bold",
